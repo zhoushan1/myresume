aa97a6e7330d1f496041b62def24df3dd5f8e568
zhoushan <672563682@qq.com>
2016-02-06 17:02:59 +0800

    修改

--- index.js
+++ index.js
@@ -53,13 +53,33 @@ function end(e) {
         this.firstElementChild.id = "a"+(this.index+1);
     })
 }
-
 window.setTimeout(function () {
     var oList = document.querySelectorAll(".a3>div");
     for (var i = 0; i < oList.length; i++) {
         oList[i].className = "move";
     }
 }, 200);
+//播放器
+var oAudio = document.querySelector("#audio1"), oDiv = document.querySelector("#div1");
+
+window.onload = function () {
+    oAudio.play();
+    //->canplay: 当前的资源文件已经下载一部分,可以播放了 canplaythrough:资源全部下载,播放过程中比较的流畅
+//    oAudio.addEventListener("canplay", function () {
+//        //oDiv.style.display = "block";
+//        oDiv.className = "theMove";
+//    }, false);
+//    //->应该用touch事件
+//    oDiv.onclick = function () {
+//        if (oAudio.paused) {
+//            oAudio.play();
+//            oDiv.className = "theMove";
+//            return;
+//        }
+//        oAudio.pause();
+//        oDiv.className = "";
+//    }
+};
 
 
 
